78766c940d6913dda1203313ff8a8164d1fe0ea4
fathur <fathur_rohman17@yahoo.co.id>
2015-04-09 09:58:20 +0700

    add theme support and remove admin bar

--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@
  * Wordpress functions boilerplate for custom theme
  */
 
+add_theme_support( "post-thumbnails" );
+add_theme_support( "title-tag" );
+add_theme_support( "custom-header", array() );
+add_theme_support( "custom-background", array() );
+add_theme_support('widgets');
+add_theme_support( 'html5', array( 'search-form', 'caption' ) );
+add_theme_support( 'automatic-feed-links' );
+
 require get_template_directory() . 'inc/remove-wordpress-brand.php';
